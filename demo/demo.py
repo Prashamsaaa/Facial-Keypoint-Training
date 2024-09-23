23eebcee89aa3a43ead6fffd49437f3f9a3613ec
Prashamsaaa <karkiprashamsa1@gmail.com>
2024-09-23 13:18:42 +0545

    add demo

---
+++ demo/demo.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '')))
+print(os.getcwd())
+
+from matplotlib import pyplot as plt
+from PIL import Image, ImageDraw
+from src.facial_key_point.utils.facial_key_point_detection import FacialKeyPointDetection
+
+st.markdown('## Facial Key Point Detection')
+
+image = st.file_uploader('Facial Image', ['jpg', 'png', 'jpeg'], accept_multiple_files = False)
+if image is not None:
+    image = Image.open(image).convert('RGB')
+    st.image(image)
+    detector = FacialKeyPointDetection()
+    image, kp = detector.predict(image)
+
+    fig = plt.figure()
+    plt.imshow(image)
+    plt.scatter(kp[0], kp[1], s=4, c='r')
+    st.pyplot(fig)
+ 
